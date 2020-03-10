808a869bd6535509e5ec1a62ecc1b74b63b4f714
Adeniran Oluwanisola <51279042+jamtechreal@users.noreply.github.com>
2020-03-10 07:39:55 +0100

    Add files via upload

---
+++ hospital.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 10, 2020 at 07:31 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospital`
+--
+CREATE DATABASE IF NOT EXISTS `hospital` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `hospital`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`email`, `password`) VALUES
+('jamtechreal@gmail.com', 'coding');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `appointment_no` int(30) NOT NULL,
+  `patient_id` int(30) NOT NULL,
+  `speciality` varchar(30) NOT NULL,
+  `medical_condition` text,
+  `doctors_suggestion` varchar(30) DEFAULT NULL,
+  `payment_amount` int(199) DEFAULT NULL,
+  `case_closed` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`appointment_no`, `patient_id`, `speciality`, `medical_condition`, `doctors_suggestion`, `payment_amount`, `case_closed`) VALUES
+(59, 85, 'Cardiologist', 'welcome', NULL, NULL, 'no');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clerks`
+--
+
+CREATE TABLE `clerks` (
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stores information about clerk';
+
+--
+-- Dumping data for table `clerks`
+--
+
+INSERT INTO `clerks` (`email`, `password`, `fullname`) VALUES
+('sunday@gmail.com', 'coding', 'Adeniran Jamiu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `speciality` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_info`
+--
+
+CREATE TABLE `patient_info` (
+  `patient_Id` int(20) NOT NULL,
+  `full_name` varchar(20) NOT NULL,
+  `DOB` int(10) NOT NULL,
+  `weight` int(8) NOT NULL,
+  `phone_no` varchar(30) NOT NULL,
+  `address` varchar(260) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='patient';
+
+--
+-- Dumping data for table `patient_info`
+--
+
+INSERT INTO `patient_info` (`patient_Id`, `full_name`, `DOB`, `weight`, `phone_no`, `address`) VALUES
+(85, 'Jamiu', 23, 45, '07060926400', 'Ishagamu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `password`, `fullname`) VALUES
+('adenirangaposa@gmail.com', 'coding', 'Adeniran Jamiu');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`appointment_no`);
+
+--
+-- Indexes for table `clerks`
+--
+ALTER TABLE `clerks`
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `patient_info`
+--
+ALTER TABLE `patient_info`
+  ADD PRIMARY KEY (`patient_Id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `username` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `appointment_no` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+
+--
+-- AUTO_INCREMENT for table `patient_info`
+--
+ALTER TABLE `patient_info`
+  MODIFY `patient_Id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
